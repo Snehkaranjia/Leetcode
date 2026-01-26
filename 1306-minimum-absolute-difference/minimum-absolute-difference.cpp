86133f70ca583fdd77ffad07741218e35ad4c90e
Snehkaranjia <163864824+Snehkaranjia@users.noreply.github.com>
2026-01-26 22:49:47 +0530

    Time: 17 ms (36.07%) | Memory: 36.8 MB (32.02%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -4,18 +4,18 @@ public:
         sort(arr.begin(), arr.end());
 
         int mindiff = INT_MAX;
-        for(int i=1; i<arr.size(); i++)
-        {
-            if(arr[i] - arr[i-1] < mindiff)
-                mindiff = arr[i] - arr[i-1];
-        }
-
         vector<vector<int>> ans;
         for(int i=1; i<arr.size(); i++)
         {
+            if(arr[i] - arr[i-1] < mindiff)
+            {
+                mindiff = arr[i] - arr[i-1];
+                ans = {};
+            }
             if(arr[i] - arr[i-1] == mindiff)
                 ans.push_back({arr[i-1], arr[i]});
         }
+
 
         return ans;
     }
